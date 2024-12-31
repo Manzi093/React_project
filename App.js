f82e1ad36944b16fe046bb1c9e5dc6f5bd2cc135
Manzi093 <manziivan453@gmail.com>
2024-12-31 12:34:21 +0200

    README.md

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to My React App</h1>
+            <MyComponent />
+        </div>
+    );
+};
+
+export default App;
+
---
+++ MyComponets.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is a React functional component.</p>
+        </div>
+    );
+};
+
+export default MyComponent;
+
